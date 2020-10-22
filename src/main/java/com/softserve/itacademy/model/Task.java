8e89b9f00c2f6f4cf2a311a47ee398bf112aaf86
Filipenkov Kyrylo <k.filipenkov@triangu.com>
2020-10-22 21:35:13 +0300

    Task model

--- src/main/java/com/softserve/itacademy/model/Task.java
+++ src/main/java/com/softserve/itacademy/model/Task.java
@@ -1,5 +1,80 @@
 package com.softserve.itacademy.model;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
-//
+
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "task_sequence"),
+                    @Parameter(name = "initial_value", value = "1"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private Long id;
+
+    @NotBlank(message = "The taskName cannot be empty.")
+    @Column(nullable = false, name = "name")
+    @Length(min = 3, max = 200)
+    private String name;
+
+    @Column(name = "priority")
+    private Priority priority;
+
+    @Column(name = "state_id")
+    private Long stateId;
+
+    @Column(name = "todo_id")
+    private Long todoId;
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Long getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(Long stateId) {
+        this.stateId = stateId;
+    }
+
+    public Long getTodoId() {
+        return todoId;
+    }
+
+    public void setTodoId(Long todoId) {
+        this.todoId = todoId;
+    }
 }
